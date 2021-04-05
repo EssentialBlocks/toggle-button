5af568687d43f74b5517c8987e95e80cf4526d57
hz-tyfoon <md.tifun@gmail.com>
2021-04-05 10:35:52 +0600

    spacing changed

--- index.js
+++ index.js
@@ -2,48 +2,48 @@ import React, { useState, useEffect } from "react";
 import "./style.scss";
 
 const ToggleButton = ({ options, focusColor, onChange, defaultSelected }) => {
-	const [selected, setSelected] = useState(defaultSelected || options[0]);
+  const [selected, setSelected] = useState(defaultSelected || options[0]);
 
-	useEffect(() => {
-		onChange(selected.value);
-	}, [selected]);
+  useEffect(() => {
+    onChange(selected.value);
+  }, [selected]);
 
-	useEffect(() => {
-		if (defaultSelected) {
-			setSelected(defaultSelected);
-		}
-	}, [defaultSelected]);
+  useEffect(() => {
+    if (defaultSelected) {
+      setSelected(defaultSelected);
+    }
+  }, [defaultSelected]);
 
-	return (
-		<div id="switch" className="eb-switch-control">
-			{options.map((option, index) => (
-				<label>
-					<input
-						className={index ? "individual" : "business"}
-						type="radio"
-						name="gh"
-						placeholder="name"
-						onChange={() => setSelected(option)}
-					/>
-					<span
-						style={{
-							color: selected.value === option.value ? "white" : "black"
-						}}
-					>
-						{option.label}
-					</span>
-				</label>
-			))}
-			<span
-				className="slideBg"
-				style={{
-					backgroundColor: "#551ef7",
-					transform:
-						selected == options[0] ? "translateX(0)" : "translateX(100%)"
-				}}
-			/>
-		</div>
-	);
+  return (
+    <div id="switch" className="eb-switch-control">
+      {options.map((option, index) => (
+        <label>
+          <input
+            className={index ? "individual" : "business"}
+            type="radio"
+            name="gh"
+            placeholder="name"
+            onChange={() => setSelected(option)}
+          />
+          <span
+            style={{
+              color: selected.value === option.value ? "white" : "black",
+            }}
+          >
+            {option.label}
+          </span>
+        </label>
+      ))}
+      <span
+        className="slideBg"
+        style={{
+          backgroundColor: "#551ef7",
+          transform:
+            selected == options[0] ? "translateX(0)" : "translateX(100%)",
+        }}
+      />
+    </div>
+  );
 };
 
 export default ToggleButton;
